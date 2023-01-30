9856d1b5f3e432aaad8f9549318b820489327840
MrAntonS <mrantonsaenko@gmail.com>
2023-01-30 17:03:47 -0500

    Checking\

--- TemplateWidget.py
+++ TemplateWidget.py
@@ -31,7 +31,6 @@ class TemplateWidget(QWidget):
             # print(i1, 'i1 after')
             if i1 in self.vars.keys():
                 continue
-            # checking
             layout = QHBoxLayout()
             layout.addSpacerItem(QSpacerItem(40, 0))
             label = QLabel(text=i1 + ":")
