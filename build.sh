ab7aebf3961f5bd04954cc123361cdab9674e51e
Anton <45099803+MrAntonS@users.noreply.github.com>
2023-03-16 17:35:59 -0400

    Changed path to universal

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 rm -f $PWD/output/Convnet
-pyinstaller --noconfirm --onefile --windowed --specpath ./output/ --distpath ./output/ --workpath ./output/build -n ConvNet "/home/bigbon/Documents/Convenient-Networking/MainWindow.py"
+pyinstaller --noconfirm --onefile --windowed --specpath ./output/ --distpath ./output/ --workpath ./output/build -n ConvNet "$PWD/MainWindow.py"
 convnet
