9fcbe078e8937692299dce42231be4356c8c7fba
Anton <45099803+MrAntonS@users.noreply.github.com>
2023-03-16 17:09:41 -0400

    Added Name to it

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
-rm -f $PWD/output/Mainwindow
-pyinstaller --noconfirm --onefile --windowed --specpath ./output/ --distpath ./output/ --workpath ./output/build  "/home/bigbon/Documents/Convenient-Networking/MainWindow.py"
+rm -f $PWD/output/Convnet
+pyinstaller --noconfirm --onefile --windowed --specpath ./output/ --distpath ./output/ --workpath ./output/build -n ConvNet "/home/bigbon/Documents/Convenient-Networking/MainWindow.py"
 convnet
