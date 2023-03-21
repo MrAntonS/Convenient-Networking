ca610f8d710e986baf095272d807cb9b00b1e9e5
Anton <45099803+MrAntonS@users.noreply.github.com>
2023-03-21 18:57:39 -0400

    Figured out how to add putty to the python process

--- Checking.py
+++ Checking.py
@@ -3,20 +3,36 @@ from PyQt5.QtGui import QWindow, QResizeEvent
 import sys
 from subprocess import Popen, PIPE, STDOUT
 import win32gui
+import win32process
 from time import sleep
+
+
+def get_hwnds_for_pid (pid):
+    def callback (hwnd, hwnds):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
+            _, found_pid = win32process.GetWindowThreadProcessId(hwnd)
+            if found_pid == pid:
+                hwnds.append(hwnd)
+        return True
+
+    hwnds = []
+    win32gui.EnumWindows(callback, hwnds)
+    return hwnds
 
 
 class QEmbed(QWidget):
     def __init__(self):
         super(QEmbed, self).__init__()
-        exePath = "putty google.com"
+        exePath = "putty telnet://F241-10-17-COMM:2031"
+        # exePath = "notepad"
         self.x = Popen(exePath)
+        print(self.x.pid)
         sleep(3)
-        hwnd = win32gui.FindWindow(None, "Putty")
-        print(hwnd, self.x.pid) 
-        window = QWindow.fromWinId(hwnd)
-        window.resize(6000, 5000)
-        self.y = self.createWindowContainer(window, self)
+        hwnd = get_hwnds_for_pid(self.x.pid)
+        hwnd1 = win32gui.FindWindow(None, "Calculator")
+        print(hwnd, hwnd1) 
+        window = QWindow.fromWinId(hwnd[-1])
+        self.y = self.createWindowContainer(window, self, )
         self.resize(1000, 600)
         self.y.resize(self.size())
         self.show()
