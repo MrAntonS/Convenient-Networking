8a7e3e39cd5c35c39ea9691a3275f40a41f8bc3e
Anton <mrantonsaenko@gmail.com>
2023-03-21 12:54:49 -0400

    Found a way to use putty on pyqt! Windows 10 only,  what a pain in the ass

--- Checking.py
+++ Checking.py
@@ -1,11 +1,30 @@
-from tkinter import *
-import os
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QWindow
+import sys
+from subprocess import Popen, PIPE, STDOUT
+import win32gui
+from time import sleep
 
-root = Tk()
-termf = Frame(root, height=400, width=500)
 
-termf.pack(fill=BOTH, expand=YES)
-wid = termf.winfo_id()
-os.system(f'xterm -into %d -geometry {400}x{500} -sb &' % wid)
+class QEmbed(QWidget):
+    def __init__(self):
+        super(QEmbed, self).__init__()
+        exePath = "putty google.com"
+        x = Popen(exePath)
+        sleep(3)
+        hwnd = win32gui.FindWindow(None, "Putty")
+        print(hwnd, x.pid) 
+        window = QWindow.fromWinId(hwnd)
+        window.resize(6000, 5000)
+        x = self.createWindowContainer(window, self)
+        self.resize(1000, 600)
+        x.resize(self.size())
+        self.show()
 
-root.mainloop()
+
+if __name__ == "__main__":
+    args = sys.argv
+    app = QApplication(args)
+    t = QEmbed()
+    t.show()
+    sys.exit(app.exec())
