d7d4fbb643a89455e60997cddd89e9d6c457c3f9
Anton <45099803+MrAntonS@users.noreply.github.com>
2023-07-04 23:02:16 -0400

    Some refactoring to the code, figured out how to track variable location and format them accordingly

--- src/main/TemplateWidget.py
+++ src/main/TemplateWidget.py
@@ -4,35 +4,53 @@ class TemplateEditor(QTextEdit):
     def __init__(self, content):
         super(TemplateEditor, self).__init__()
         self.content = content
+        self.setHtml(content)
+        self.startTimer(1000)
+
+    def timerEvent(self, e: QTimerEvent) -> None:
+        print(self.textCursor().position())
+        print(self.toHtml())
+        return super().timerEvent(e)
 
     def keyPressEvent(self, e: QKeyEvent) -> None:
-        
         return super().keyPressEvent(e)
 
     def formatText(self, content):
         #TODO write a script to format text with syntax highlighting and auto replacement for variables
-        pass
-class TemplateWidget(QWidget):
-    def __init__(self, content, host):
-        super(TemplateWidget, self).__init__()
-        self.vars = dict()
-        self.hostname = host
-        self.initUI(content)
+        result_string = ""
+        
+        
         pass
 
-    def initUI(self, content):
-        self.mainLayout = QHBoxLayout()
+class variableWidget(QWidget):
+    def __init__(self, content):
+        super(variableWidget, self).__init__()
         self.Vbox = QVBoxLayout()
-        self.setLayout(self.mainLayout)
-        self.setWindowTitle(self.hostname)
-        self.templateEditor = TemplateEditor(content)
-        self.templateEditor.setContentsMargins(0, 0, 0, 0)
-        self.mainLayout.addWidget(self.templateEditor)
-        self.mainLayout.addLayout(self.Vbox)
+        self.setLayout(self.Vbox)
+        variables = variableWidget.getAllVariables(content)
+        for variable in variables:
+            layout = QHBoxLayout()
+            layout.addSpacerItem(QSpacerItem(40, 0))
+            label = QLabel(text=variable + ":")
+            line_edit = QLineEdit()
+            line_edit.setText(variables[variable])
+            layout.addWidget(label)
+            layout.addWidget(line_edit)
+            layout.addSpacerItem(QSpacerItem(40, 0))
+            self.Vbox.addLayout(layout)
+        layout = QHBoxLayout()
+        layout.addSpacerItem(QSpacerItem(40, 0))
+        self.submit = QPushButton(text="Submit")
+        layout.addWidget(self.submit)
+        layout.addSpacerItem(QSpacerItem(40, 0))
+        self.Vbox.addLayout(layout)
+
+    def getAllVariables(content):
+        vars = dict()
         p = re.compile(r'({{[^}]+}})')
         all_vars = re.findall(p, ''.join(content))
         for variable in all_vars:
-            if variable in self.vars.keys():
+            if variable in vars.keys():
                 continue
             title = variable[2:-2]
             title = title.split('|')
@@ -42,25 +60,27 @@ class TemplateWidget(QWidget):
             else:
                 suggested = title[1]
                 title = title[0]
-            if title in self.vars.keys():
+            if title in vars.keys():
                 continue
-            layout = QHBoxLayout()
-            layout.addSpacerItem(QSpacerItem(40, 0))
-            label = QLabel(text=title + ":")
-            line_edit = QLineEdit()
-            line_edit.setText(suggested)
-            layout.addWidget(label)
-            layout.addWidget(line_edit)
-            layout.addSpacerItem(QSpacerItem(40, 0))
-            self.vars[i] = line_edit
-            self.vars["{{" + title + "}}"] = line_edit
-            self.Vbox.addLayout(layout)
-        layout = QHBoxLayout()
-        layout.addSpacerItem(QSpacerItem(40, 0))
-        self.submit = QPushButton(text="Submit")
-        layout.addWidget(self.submit)
-        layout.addSpacerItem(QSpacerItem(40, 0))
-        self.Vbox.addLayout(layout)
+            vars[title] = suggested
+        return vars
+class TemplateWidget(QWidget):
+    def __init__(self, content, host):
+        super(TemplateWidget, self).__init__()
+        self.hostname = host
+        self.initUI(content)
+        pass
+
+    def initUI(self, content):
+        self.mainLayout = QHBoxLayout()
+        self.setLayout(self.mainLayout)
+        self.setWindowTitle(self.hostname)
+
+        self.templateEditor = TemplateEditor(content)
+        self.mainLayout.addWidget(self.templateEditor)
+
+        self.varWidget = variableWidget(content)
+        self.mainLayout.addWidget(self.varWidget)
 
     def keyPressEvent(self, a0: QKeyEvent) -> None:
         if a0.key() == Qt.Key.Key_Return:
